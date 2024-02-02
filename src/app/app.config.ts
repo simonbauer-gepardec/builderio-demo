88f1c7856a496806ccf10472b2d32e3613e3df1c
Simon Bauer <simon.bauer@gepardec.com>
2024-02-02 12:05:03 +0100

    add BuilderModule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, inject} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {BuilderModule} from '@builder.io/angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(BuilderModule.forRoot("TODO"))],
 };
